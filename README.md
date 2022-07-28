5c2c70dfecb8c86df603e15140f8dc5fecb48280
Ross <ross.gosling@btinternet.com>
2022-07-28 19:30:35 +0100

    remove prior class after application of new

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-# ToggleVideoProgressBars
+# ToggleWatchedVideos
 
 ## Description
-Provides a context menu button, as well as a standard browser icon button, that when pressed toggles the visibility of progress bars and displayed time for various video players!
-
-Supports YouTube, Netflix, Amazon, and Vimeo
+Provides a context menu button, as well as a standard browser icon button, that when pressed toggles the visibility of videos you have watched on YouTube
 
 ## Donate?
 [![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=5DQNUQQDKWYEA)
--- src/document.js
+++ src/document.js
@@ -32,21 +32,6 @@
         const setHidden = () => {
             try {
 
-                // Remove old stylesheet
-
-                const oldElement = document.getElementById(UNIQUE_ID);
-
-                if (oldElement) {
-
-                    oldElement.parentNode.removeChild(oldElement);
-                }
-
-                // Error handling if there were more elements for some reason
-                if (document.getElementById(UNIQUE_ID)) {
-
-                    return setObscured();
-                }
-
                 // Add new stylesheet
 
                 const element = document.createElement("link");
@@ -58,15 +43,6 @@
 
                 document.body.appendChild(element);
 
-
-            } catch (error) {
-                console.error(error);
-            }
-        }
-
-        const setObscured = () => {
-            try {
-
                 // Remove old stylesheet
 
                 const oldElement = document.getElementById(UNIQUE_ID);
@@ -82,6 +58,14 @@
                     return setObscured();
                 }
 
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        const setObscured = () => {
+            try {
+
                 // Add new stylesheet
 
                 const element = document.createElement("link");
@@ -92,6 +76,21 @@
                 element.setAttribute("href", browser.runtime.getURL(`/style/Obscured.css`));
 
                 document.body.appendChild(element);
+
+                // Remove old stylesheet
+
+                const oldElement = document.getElementById(UNIQUE_ID);
+
+                if (oldElement) {
+
+                    oldElement.parentNode.removeChild(oldElement);
+                }
+
+                // Error handling if there were more elements for some reason
+                if (document.getElementById(UNIQUE_ID)) {
+
+                    return setObscured();
+                }
 
             } catch (error) {
                 console.error(error);
